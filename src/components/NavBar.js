c9e556cec1e6ba2696493d35a2b2e3295199f54c
Miguel <mirkouda@gmail.com>
2023-10-31 17:30:45 +0100

    Banner done

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -48,9 +48,9 @@ function NavBar() {
                     </Nav>
                     <span className='navbar-text'>
                         <div className='social-icon'>
-                            <a href='#'><img src={navIcon1} alt="" /></a>
-                            <a href='#'><img src={navIcon2} alt="" /></a>
-                            <a href='#'><img src={navIcon3} alt="" /></a>
+                            <a href="#"><img src={navIcon1} alt="" /></a>
+                            <a href="#"><img src={navIcon2} alt="" /></a>
+                            <a href="#"><img src={navIcon3} alt="" /></a>
                         </div>
                         <button className='vvd' onClick={() => console.log('connect')}>
                             <span>Let's connect</span>
