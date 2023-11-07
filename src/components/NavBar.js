e93c9746e301ee558f791b3ab6476738e65d26eb
Miguel <mirkouda@gmail.com>
2023-11-07 17:16:50 +0100

    Some assets changed

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,6 +4,7 @@ import logo from '../assets/img/logo.svg'
 import navIcon1 from '../assets/img/nav-icon1.svg'
 import navIcon2 from '../assets/img/nav-icon2.svg'
 import navIcon3 from '../assets/img/nav-icon3.svg'
+import navIcon4 from '../assets/img/nav-icon4.svg'
 
 function NavBar() {
 
@@ -46,6 +47,7 @@ function NavBar() {
                             <a href="https://www.linkedin.com/in/mherrevi/"><img src={navIcon1} alt="" /></a>
                             <a href="https://github.com/miguelherreravillanueva"><img src={navIcon2} alt="" /></a>
                             <a href="https://www.instagram.com/miki.hv/"><img src={navIcon3} alt="" /></a>
+                            <a href="https://www.instagram.com/miki.hv/"><img src={navIcon4} alt="" /></a>
                         </div>
                         <button className='vvd' onClick={() => console.log('connect')}>
                             <span>Let's connect</span>
